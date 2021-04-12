4bbf8ba5f9c2125789819408fc9064862e287f46
Oleg Bezriadin <muhachov86@gmail.com>
2021-04-12 08:26:22 +0300

    Updates

---
+++ precache-manifest.d83e51f9ec320fa99b105f953b62ae54.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f200d06fff6da217edd63c696f2f7c94",
+    "url": "./index.html"
+  },
+  {
+    "revision": "df3047c187283a7db140",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "194499448c43c59fc8e6",
+    "url": "./static/css/main.6ce43aae.chunk.css"
+  },
+  {
+    "revision": "df3047c187283a7db140",
+    "url": "./static/js/2.f9f667a8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f9f667a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "194499448c43c59fc8e6",
+    "url": "./static/js/main.958a68f5.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
